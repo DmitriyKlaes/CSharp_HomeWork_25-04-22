696420c11bd756bc2059f32e68fe4cbb08cd78ab
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-02 17:09:24 +0300

    Task000 определение большего из двух чисел

---
+++ Task000/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите число: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 != number2)
+{
+    Console.Write(number1);
+    if (number1 > number2)
+    {
+        Console.Write(" > ");
+    }
+    else 
+    {
+        Console.Write(" < ");
+    }
+    Console.Write(number2);
+}
+else
+{
+    Console.Write("Числа равны");
+}
